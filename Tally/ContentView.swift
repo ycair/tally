a532112e01c747f74eb9cf7bc62f4c6ff6d8805f
YCAIR <ian9302128989@gmail.com>
2026-07-23 03:05:24 +0800

    chore: scaffold Xcode project with SwiftData, disable CloudKit, model stubs

---
+++ Tally/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("一筆 / Tally")
+            .font(.largeTitle)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tally/TallyApp.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+import SwiftData
+
+@main
+struct TallyApp: App {
+    static let container: ModelContainer = {
+        let schema = Schema([Expense.self, LineItem.self, FixedCost.self,
+                             IncomeEvent.self, MoneyJar.self, DailyBudget.self,
+                             AppSettings.self])
+        let config = ModelConfiguration(cloudKitDatabase: .none)
+        return try! ModelContainer(for: schema, configurations: config)
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(Self.container)
+    }
+}
